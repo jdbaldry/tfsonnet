48381c5fc3eb528728de81d62d0adec840ea59a9
jdb <jack.baldry@oracle.com>
2020-03-10 00:33:07 +0000

    Reorder structs

--- schema.go
+++ schema.go
@@ -1,5 +1,26 @@
 package main
 
+// Schema models the useful output from `terraform providers schema -json`.
+type Schema struct {
+	ProviderSchemas map[string]ProviderSchema `json:"provider_schemas"`
+}
+
+// ProviderSchema models a Terraform provider schema.
+type ProviderSchema struct {
+	ResourceSchemas map[string]ResourceSchema `json:"resource_schemas"`
+}
+
+// ResourceSchema models a Terraform resource schema.
+type ResourceSchema struct {
+	Block Block
+}
+
+// Block models a Terraform HCL block.
+type Block struct {
+	Attributes map[string]Attribute
+	BlockTypes *map[string]BlockType `json:"block_types"`
+}
+
 // Attribute models a Terraform attribute.
 // TODO: Handle type field.
 type Attribute struct {
@@ -14,25 +35,4 @@ type BlockType struct {
 	MinItems    int    `json:"min_items"`
 	MaxItems    int    `json:"max_items"`
 	NestingMode string `json:"nesting_mode"`
-}
-
-// Block models a Terraform HCL block.
-type Block struct {
-	Attributes map[string]Attribute
-	BlockTypes *map[string]BlockType `json:"block_types"`
-}
-
-// ResourceSchema models a Terraform resource schema.
-type ResourceSchema struct {
-	Block Block
-}
-
-// ProviderSchema models a Terraform provider schema.
-type ProviderSchema struct {
-	ResourceSchemas map[string]ResourceSchema `json:"resource_schemas"`
-}
-
-// Schema models the useful output from `terraform providers schema -json`.
-type Schema struct {
-	ProviderSchemas map[string]ProviderSchema `json:"provider_schemas"`
 }
